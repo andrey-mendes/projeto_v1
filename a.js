feb93288ee563bed686aacce80781a4cc0bec231
Andrey Mendes <andreylsm@gmail.com>
2022-09-12 08:36:38 -0300

    feature/1.0.1.pontos

--- a.js
+++ a.js
@@ -1,8 +1,13 @@
 //Jogo par impar
 
 let mensagem;
+let pontos
 
 function jogoParImpar (arguments) {
     n ? mensagem = "Jogar!" : null;    
     return mensagem;
+}
+
+function pontos () {
+    pontos++;
 }
